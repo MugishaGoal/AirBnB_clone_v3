6c4f4b6da7041a9bf03ba4269d9f8637774ccd43
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-09 14:15:36 -0800

    APIs

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -70,7 +70,7 @@ def add_state(state_id=None):
 def update_state(state_id=None):
     '''Updates the state with the given id.
     '''
-    xkeys = ('id', 'created_at', 'updated_at')
+    un_keys = ('id', 'created_at', 'updated_at')
     all_states = storage.all(State).values()
     res = list(filter(lambda x: x.id == state_id, all_states))
     if res:
@@ -79,7 +79,7 @@ def update_state(state_id=None):
             raise BadRequest(description='Not a JSON')
         old_state = res[0]
         for key, value in data.items():
-            if key not in xkeys:
+            if key not in un_keys:
                 setattr(old_state, key, value)
         old_state.save()
         return jsonify(old_state.to_dict()), 200
