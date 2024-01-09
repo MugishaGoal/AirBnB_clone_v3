1eeeae965b94434bf4d5d80535e21da24bcc6e6c
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-09 12:56:42 -0800

    APIs

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Contains Blueprint for the API"""
+"""Has Blueprint for the API"""
 from flask import Blueprint
 
 
@@ -8,3 +8,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
+from api.v1.views.states import *
